c658b214f04ff0a06729081971039ebebf80d1b8
shadyn <shadyndyn234@gmail.com>
2024-10-24 15:12:59 +0200

    subida

---
+++ src/main/java/com/bookjournal/proyecto/ShadinProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.bookjournal.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadinProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadinProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookjournal/proyecto/ShadinProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookjournal.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadinProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
