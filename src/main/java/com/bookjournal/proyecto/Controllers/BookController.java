fabb6c2e93de1757a45d9986205f8895ceef8f80
shadyn <shadyndyn234@gmail.com>
2024-10-25 19:45:27 +0200

    subida de todo correcto por ahorita

--- src/main/java/com/bookjournal/proyecto/Controllers/BookController.java
+++ src/main/java/com/bookjournal/proyecto/Controllers/BookController.java
@@ -2,13 +2,22 @@ package com.bookjournal.proyecto.Controllers;
 
 import com.bookjournal.proyecto.Services.BookService;
 import com.bookjournal.proyecto.entities.Book;
+import com.bookjournal.proyecto.entities.User;
 import com.bookjournal.proyecto.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -16,12 +25,26 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
-    @GetMapping("/user/{userId}")
-    public List<Book> getBooksByUserId(@PathVariable Long userId){
-        return bookService.getBooksByUserId(userId);
-    }
     @Autowired
     private BookRepository bookRepository;
+
+    private final String uploadDir = "uploads/"; // Directorio donde se guardarán las imágenes
+
+    @GetMapping("/user/{userId}")
+    public List<Book> getBooksByUserId(@PathVariable Long userId) {
+        return bookService.getBooksByUserId(userId);
+    }
+
+
+    @GetMapping("/")
+    public List<Book> getAllUsers() {
+        return bookService.getAllBooks();
+    }
+
+    @DeleteMapping("/{bookId}")
+    public void deleteBook(@PathVariable Long bookId) {
+        bookService.deleteBook(bookId);
+    }
 
     @PostMapping("/create")
     public ResponseEntity<Book> createBook(@RequestBody Book book) {
@@ -33,17 +56,7 @@ public class BookController {
             System.out.println("Error al crear el libro: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+
     }
 
-
-
-    @GetMapping("/")
-    public List<Book> getAllUsers() {
-        return bookService.getAllBooks(); // Devuelve todos los usuarios
-    }
-
-    @DeleteMapping("/{bookId}")
-    public void deleteBook(@PathVariable Long bookId){
-        bookService.deleteBook(bookId);
-    }
 }
