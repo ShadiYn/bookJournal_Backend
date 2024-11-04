c644245330faa4ec97a4521cf3a5e56dc9b5d7fc
shadin <shadyndyn@gmail.com>
2024-11-04 16:54:46 +0100

    mensajeria correcto

--- src/main/java/com/bookjournal/proyecto/Mensajeria/repositories/MensajeRepository.java
+++ src/main/java/com/bookjournal/proyecto/Mensajeria/repositories/MensajeRepository.java
@@ -1,13 +1,23 @@
 package com.bookjournal.proyecto.Mensajeria.repositories;
 
 import com.bookjournal.proyecto.Mensajeria.Entities.Mensaje;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface MensajeRepository extends CrudRepository<Mensaje, Integer> {
+// MensajeRepository.java
+@Repository
+public interface MensajeRepository extends JpaRepository<Mensaje, Long> {
+    List<Mensaje> findByRemitenteIdOrDestinatarioId(Long remitenteId, Long destinatarioId);
+
+
+
+
+
 
 
     List<Mensaje> findAll();
--- src/main/java/com/bookjournal/proyecto/Mensajeria/repositories/UsuarioRepository.java
+++ src/main/java/com/bookjournal/proyecto/Mensajeria/repositories/UsuarioRepository.java
@@ -1,13 +1,17 @@
 package com.bookjournal.proyecto.Mensajeria.repositories;
 
 import com.bookjournal.proyecto.Mensajeria.Entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    List<Usuario> findByIdIn(List<Long> ids);
 
     @Query("SELECT u FROM Usuario u WHERE u.nombre=:nombre")
     Usuario findUsuario(@Param("nombre")String nombre);
